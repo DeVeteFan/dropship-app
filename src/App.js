56304d35a7779c2bc90f44d25d635fdde2fd1622
pontussandberg <sandbergpontus97@gmail.com>
2020-01-08 17:32:33 +0100

    adding basics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import NavBar from './components/nav-bar';
 import Homepage from './components/homepage';
+import Footer from './components/footer';
 import './css/app.css';
+import './css/vars.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <React.Fragment>
       <NavBar />
       <Homepage />
+      <Footer />
     </React.Fragment>
   );
 }
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Featured from './featured';
+import '../css/homepage.css';
 
 class Homepage extends Component {
     state = {}
     render() {
         return (
-            <div className="container">
-                <Featured />
-            </div>
+            <React.Fragment>
+                <div className="black-bg">
+                    <div className="container">
+                        <Featured />
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="flex-wrapper">
+                        <div className="card-primary"></div>
+                        <div className="card-primary"></div>
+                        <div className="card-primary"></div>
+                        <div className="card-primary"></div>
+                        <div className="card-primary"></div>
+                        <div className="card-primary"></div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
